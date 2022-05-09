60faa2ff6b70ece8ddd6aef3df11ea6d58f1cad9
Guilherme <guilhaugusto05@gmail.com>
2022-05-09 00:46:56 -0300

    chore: update app with new features
    add error middleware and routes

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const errorMiddleware = require('../middlewares/error');
+
+const routes = require('../routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'Hello!' }));
+app.use(routes);
 
-app.use(cors());
+app.use(errorMiddleware);
 
 module.exports = app;

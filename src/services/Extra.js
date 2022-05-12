567124198ee375ac42c18fd9598408568154f84a
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 18:41:35 -0300

    feat: create ExtraService class with methods below
    create and getById

---
+++ src/services/Extra.js
@@ -0,0 +1,40 @@
+const { Extra, Character } = require('../database/models');
+const { RequestError, httpCodes, helpers } = require('../utils');
+
+const messages = {
+  characterNotFound: 'Character not found',
+  extraAlreadyRegistered: 'Extra informations already registered',
+  extraNotFound: 'Extra informations not found',
+};
+
+class ExtraService {
+  static async create({ characterId, info }, method) {
+    const existingCharacter = await Character.findByPk(characterId);
+    if (!existingCharacter) throw new RequestError(
+      messages.characterNotFound, httpCodes.NOT_FOUND,
+    );
+
+    const existingExtra = await Extra.findByPk(characterId);
+    if (method === 'PUT' && !existingExtra) throw new RequestError(
+      messages.extraNotFound, httpCodes.NOT_FOUND
+    );
+    if (method === 'POST' && existingExtra) throw new RequestError(
+      messages.extraAlreadyRegistered, httpCodes.CONFLICT
+    );
+
+    const extra = await (method === 'POST'
+      ? Extra.create({ characterId, info })
+      : existingExtra.update({ info }));
+
+    return extra;
+  }
+
+  static async getById(characterId) {
+    const extra = await Extra.findByPk(characterId);
+
+    if (!extra) throw new RequestError(messages.extraNotFound, httpCodes.NOT_FOUND);
+    return extra;
+  }
+};
+
+module.exports = ExtraService;
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const StoryService = require('./Story');
 const CharacterService = require('./Character');
 const KeyPointsService = require('./KeyPoints');
+const ExtraService = require('./Extra');
 
-module.exports = { StoryService, CharacterService, KeyPointsService };
+module.exports = { StoryService, CharacterService, KeyPointsService, ExtraService };

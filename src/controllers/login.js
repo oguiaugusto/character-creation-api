2c2d1d3d7ab1a069102b4b190f29e9b7f2fbde73
Guilherme <guilhaugusto05@gmail.com>
2022-05-13 12:29:15 -0300

    fix: throws request error if login fails

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,13 +1,16 @@
 const { UserService } = require('../services');
-const { httpCodes, jwtUser } = require('../utils');
+const { RequestError, httpCodes, jwtUser } = require('../utils');
 
 module.exports = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
     const user = await UserService.getByCredentials({ email, password });
-    const token = jwtUser.sign(user);
+    if (!user) throw new RequestError(
+      'User does not exist or invalid password', httpCodes.UNAUTHORIZED,
+    );
 
+    const token = jwtUser.sign(user);
     return res.status(httpCodes.OK).json({ token });
   } catch (error) {
     next(error);

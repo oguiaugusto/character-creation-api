c960ed05f3a479e2e1f6ed8cec90181c098ba273
Guilherme <guilhaugusto05@gmail.com>
2022-05-13 12:37:14 -0300

    feat: environment shown in GET / endpoint

--- src/api/app.js
+++ src/api/app.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 const errorMiddleware = require('../middlewares/error');
@@ -8,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => res.status(200).json({ message: 'Hello!' }));
+app.get('/', (_req, res) => res
+  .status(200)
+  .json({ message: 'Hello!', environment: process.env.NODE_ENV }));
 app.use(routes);
 
 app.use(errorMiddleware);

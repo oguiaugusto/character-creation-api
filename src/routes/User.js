878af87e9a87750c34ac9f2dc0725b8393324ff3
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 22:01:33 -0300

    feat: create User routes and endpoints

---
+++ src/routes/User.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { UserController, login } = require('../controllers');
+const auth = require('../middlewares/auth');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+router
+  .route('/')
+  .post(
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().email().not().empty().required(),
+        password: Joi.string().not().empty().required(),
+      }),
+    }), UserController.create);
+
+router
+  .route('/login')
+  .post(
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().email().not().empty().required(),
+        password: Joi.string().not().empty().required(),
+      }),
+    }), login);
+
+router
+  .route('/:id')
+  .get(auth, UserController.getById)
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const StoryRouter = require('./Story');
 const CharacterRouter = require('./Character');
+const UserRouter = require('./User');
 
 router.use('/stories', StoryRouter);
 router.use('/characters', CharacterRouter);
+router.use('/users', UserRouter);
 
 module.exports = router;

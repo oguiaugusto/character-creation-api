6f15a910eea8ff87a14fbc0b95102d5ac39e1eb4
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 21:57:36 -0300

    chore: jwtUser is imported from utils index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 const RequestError = require('./RequestError');
 const httpCodes = require('./httpCodes');
 const helpers = require('./helpers');
+const jwtUser = require('./jwtUser');
 
-module.exports = { RequestError, httpCodes, helpers };
+module.exports = { RequestError, httpCodes, helpers, jwtUser };

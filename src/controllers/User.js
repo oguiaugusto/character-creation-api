e898c2b2b2a2cca8adb08b3c2614e3b176b04712
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 22:20:08 -0300

    feat: creation verification
    it can only be created with the correct Authorization

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,11 +1,24 @@
+require('dotenv/config');
 const { UserService } = require('../services');
 const { httpCodes, jwtUser } = require('../utils');
 const { RequestError } = require('../utils');
 
+const { CREATION_PASSWORD } = process.env;
+
+const messages = {
+  unauthorized: 'Unauthorized access',
+};
+
 class UserController {
   static async create(req, res, next) {
     try {
-      const { email, password } = req.body;
+      const { body: { email, password }, headers: { authorization } } = req;
+
+      console.log(authorization.toString());
+      console.log(CREATION_PASSWORD);
+      if (authorization !== CREATION_PASSWORD) throw new RequestError(
+        messages.unauthorized, httpCodes.UNAUTHORIZED
+      );
 
       const user = await UserService.create({ email, password });
       const token = jwtUser.sign(user);
@@ -34,7 +47,7 @@ class UserController {
       const user = await UserService.getById(req.params.id);
 
       if (!req.user.admin && req.params.id !== req.user.id) throw new RequestError(
-        'Unauthorized access', httpCodes.UNAUTHORIZED
+        messages.unauthorized, httpCodes.UNAUTHORIZED
       );
       return res.status(httpCodes.OK).json(user);
     } catch (error) {

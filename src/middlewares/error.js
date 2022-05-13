102a500c27c4f254b4c5bb2712d332381da44004
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 22:02:14 -0300

    feat: update error middleware
    it handles the jwt expired error

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,9 +2,16 @@ const { isCelebrateError } = require('celebrate');
 const { httpCodes } = require('../utils');
 
 module.exports = (err, _req, res, _next) => {
+  console.log(err.message, err.expiredAt);
   if (isCelebrateError(err)) {
     const { message } = err.details.entries().next().value[1].details[0];
     return res.status(httpCodes.BAD_REQUEST).json({ message });
+  }
+
+  if (err.name === 'TokenExpiredError') {
+    return res
+      .status(httpCodes.UNAUTHORIZED)
+      .json({ message: 'Token expired', expiredAt: err.expiredAt.toTimeString() });
   }
 
   if (!err.code || !err.message) {

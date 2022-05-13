46d301cd4637986eb04169d426c02fc90c0c7618
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 21:59:55 -0300

    feat: create login controller

---
+++ src/controllers/login.js
@@ -0,0 +1,15 @@
+const { UserService } = require('../services');
+const { httpCodes, jwtUser } = require('../utils');
+
+module.exports = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    
+    const user = await UserService.getByCredentials({ email, password });
+    const token = jwtUser.sign(user);
+
+    return res.status(httpCodes.OK).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};

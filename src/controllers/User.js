be8185ef753e2fbd306eb1f66b52f03592e850d5
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 22:00:33 -0300

    feat: create UserController with the following methods:
    create, update and getById

---
+++ src/controllers/User.js
@@ -0,0 +1,46 @@
+const { UserService } = require('../services');
+const { httpCodes, jwtUser } = require('../utils');
+const { RequestError } = require('../utils');
+
+class UserController {
+  static async create(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await UserService.create({ email, password });
+      const token = jwtUser.sign(user);
+
+      return res.status(httpCodes.CREATED).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await UserService.update({ email, password });
+      const token = jwtUser.sign(user);
+
+      return res.status(httpCodes.OK).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getById(req, res, next) {
+    try {
+      const user = await UserService.getById(req.params.id);
+
+      if (!req.user.admin && req.params.id !== req.user.id) throw new RequestError(
+        'Unauthorized access', httpCodes.UNAUTHORIZED
+      );
+      return res.status(httpCodes.OK).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
+
+module.exports = UserController;
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,14 @@ const StoryController = require('./Story');
 const CharacterController = require('./Character');
 const KeyPointsController = require('./KeyPoints');
 const ExtraController = require('./Extra');
+const UserController = require('./User');
+const login = require('./login');
 
-module.exports = { StoryController, CharacterController, KeyPointsController, ExtraController };
+module.exports = {
+  StoryController,
+  CharacterController,
+  KeyPointsController,
+  ExtraController,
+  UserController,
+  login,
+};

d8af6538c22b5f657c45a268aa68c5b956f395cb
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 21:54:57 -0300

    feat: create User model

---
+++ src/database/migrations/20220512232231-create-user.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING(8),
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+  User.init({
+    id: { type: DataTypes.STRING(8), primaryKey: true },
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    timestamps: false,
+  });
+  return User;
+};

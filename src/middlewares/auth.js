166bec9b3ef11015c6e26bcc114cf24dcebc832c
Guilherme <guilhaugusto05@gmail.com>
2022-05-12 21:56:13 -0300

    feat: create auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const { User } = require('../database/models');
+const { RequestError, httpCodes, jwtUser } = require('../utils');
+
+const messages = {
+  tokenNotFound: 'Token not found',
+  noUser: 'Error finding token user',
+};
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers['authorization'];
+    if (!token) throw new RequestError(messages.tokenNotFound, httpCodes.UNAUTHORIZED);
+
+    const decoded = jwtUser.verify(token);
+
+    const user = await User.findOne({ where: { email: decoded.email } });
+
+    if (!user) throw new RequestError(messages.noUser);
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};

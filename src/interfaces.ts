c200b3b6a3e9921346d77851c2d2ab866eae2d17
Guilherme <guilhaugusto05@gmail.com>
2022-05-27 13:00:27 -0300

    feat: create IUserDTO and IUserDTOWithId interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,6 +2,16 @@ export interface IUser {
   id: string;
   username: string;
   password: string;
-  picture: string;
+  picture: string | null;
   admin: boolean;
+}
+
+export interface IUserDTO {
+  username: string;
+  password: string;
+  picture: string | null;
+}
+
+export interface IUserDTOWithId extends IUserDTO {
+  id: string;
 }

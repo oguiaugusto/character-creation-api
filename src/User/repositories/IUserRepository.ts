d4abc84d1efce44c142b4a2cc645776f1fefb300
Guilherme <guilhaugusto05@gmail.com>
2022-05-27 12:59:48 -0300

    refractor: IUserRepository has separate interfaces

--- src/User/repositories/IUserRepository.ts
+++ src/User/repositories/IUserRepository.ts
@@ -1,9 +1,26 @@
-import { IUser } from '../../interfaces';
+import { IUser, IUserDTOWithId } from '../../interfaces';
 
-export interface IUserRepository {
-  create: (user: IUser) => Promise<IUser>;
-  // update: (user: IUser) => Promise<IUser>;
-  // remove: (id: string) => Promise<IUser>;
-  // findById: (id: string) => Promise<IUser>;
-  // findAll: () => Promise<IUser[]>;
+export interface ICreateUserRepository {
+  findAllIds: () => Promise<string[]>;
+  create: (user: IUserDTOWithId) => Promise<IUser | null>;
 }
+
+export interface IUpdateUserRepository {
+  update: (user: IUser) => Promise<IUser>;
+}
+export interface IRemoveUserRepository {
+  remove: (id: string) => Promise<IUser>;
+}
+export interface IFindByIdUserRepository {
+  findById: (id: string) => Promise<IUser>;
+}
+export interface IFindAllUserRepository {
+  findAll: () => Promise<IUser[]>;
+}
+
+export interface IUserRepository extends
+ICreateUserRepository,
+IUpdateUserRepository,
+IRemoveUserRepository,
+IFindByIdUserRepository,
+IFindAllUserRepository {}

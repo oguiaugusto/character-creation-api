cdf125ed585d19973257214cac5f7cca381f9862
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-06 16:35:19 -0300

    feat: class implements user repository methods in memory

---
+++ src/repositories/MemoryUserRepository.ts
@@ -0,0 +1,23 @@
+import { v4 } from 'uuid';
+import { IUser, IUserDTO } from '../interfaces/IUser';
+import { IUserRepository } from './IUserRepository';
+
+class MemoryUserRepository implements IUserRepository {
+  private users: IUser[] = [];
+
+  public create = async (user: IUserDTO): Promise<IUser> => {
+    const newUser = { id: v4(), ...user };
+    this.users.push(newUser);
+
+    return newUser;
+  };
+
+  public findByUsername = async (username: string): Promise<IUser | null> => {
+    const user = this.users.find((u) => u.username === username);
+    if (!user) return null;
+
+    return user;
+  };
+}
+
+export default MemoryUserRepository;
---
+++ src/repositories/index.ts
@@ -0,0 +1,5 @@
+import MemoryUserRepository from './MemoryUserRepository';
+
+const memoryUserRepository = new MemoryUserRepository();
+
+export { memoryUserRepository };

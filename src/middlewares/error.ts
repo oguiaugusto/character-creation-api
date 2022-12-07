477963fba8cfc109c6ff51ac782e69d48cd0b042
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-06 21:47:33 -0300

    feat: error middleware handles celebrate errors

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,13 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
+import { CelebrateError, isCelebrateError } from 'celebrate';
 import { StatusCodes } from 'http-status-codes';
 import { Messages, RequestError } from '../utils';
 
-type ErrType = RequestError;
+type ErrType = RequestError | CelebrateError;
 
 class ErrorMiddleware {
-  static handle = (err: ErrType, _req: Request, res: Response, _next: NextFunction) => {
+  private static getCelebrateValues(err: CelebrateError) {
+    const { message } = err
+      .details.entries().next().value[1].details[0];
+    const status = message.includes('must ')
+      ? StatusCodes.UNPROCESSABLE_ENTITY : StatusCodes.BAD_REQUEST;
+
+    return { message, status };
+  }
+
+  public static handle = (err: ErrType, _req: Request, res: Response, _next: NextFunction) => {
     let status = StatusCodes.INTERNAL_SERVER_ERROR;
-    let message = Messages.INTERNAL_SERVER_ERROR;
+    let message: Messages | string = Messages.INTERNAL_SERVER_ERROR;
+
+    if (isCelebrateError(err)) {
+      const { message: celMessage, status: celStatus } = ErrorMiddleware.getCelebrateValues(err);
+      status = celStatus;
+      message = celMessage;
+    }
 
     if (err instanceof RequestError) {
       status = err.status;

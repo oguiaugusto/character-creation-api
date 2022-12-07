f6f340bf75ac9d413523e898775021d7c778e938
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-06 22:33:28 -0300

    chore: new error messages
    user not found and invalid password

--- src/utils/Messages.ts
+++ src/utils/Messages.ts
@@ -1,6 +1,8 @@
 enum Messages {
   INTERNAL_SERVER_ERROR = 'Internal Server Error',
   USER_ALREADY_EXISTS = 'User already exists',
+  USER_NOT_FOUND_USERNAME = 'User not found, check your username',
+  INVALID_PASSWORD = 'Invalid password',
 }
 
 export default Messages;

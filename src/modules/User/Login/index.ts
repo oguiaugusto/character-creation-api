d042d89e15171c40c6c47cbaf35e320258bfc2a1
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-06 22:35:43 -0300

    feat: POST /users/login

---
+++ src/modules/User/Login/index.ts
@@ -0,0 +1,12 @@
+import { memoryUserRepository } from '../../../repositories';
+import jwtUser from '../../../utils/JWTUser';
+import UserLoginController from './UserLoginController';
+import UserLoginService from './UserLoginService';
+
+const userLoginService = new UserLoginService(
+  memoryUserRepository,
+  jwtUser,
+);
+const userLoginController = new UserLoginController(userLoginService);
+
+export { userLoginController };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,7 @@ import { celebrate, Segments } from 'celebrate';
 import rescue from 'express-rescue';
 import { userCreateController } from '../modules/User/Create';
 import Schemas from '../utils/JoiSchemas';
+import { userLoginController } from '../modules/User/Login';
 
 const userRouter = Router();
 
@@ -11,6 +12,13 @@ userRouter
   .post(
     celebrate({ [Segments.BODY]: Schemas.createUser }),
     rescue(userCreateController.handle),
+  );
+
+userRouter
+  .route('/login')
+  .post(
+    celebrate({ [Segments.BODY]: Schemas.createUser }), // It's the same schema for now
+    rescue(userLoginController.handle),
   );
 
 export default userRouter;

cabcf4380249ecded8eda189592b48cf5b607d86
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-10 16:35:06 -0300

    feat: username must be lowercase

--- src/utils/JoiSchemas.ts
+++ src/utils/JoiSchemas.ts
@@ -3,7 +3,7 @@ import { Joi } from 'celebrate';
 class Schemas {
   public static get createUser() {
     return Joi.object().keys({
-      username: Joi.string().min(3).max(20).required(),
+      username: Joi.string().min(3).max(20).lowercase().required(),
       password: Joi.string().min(6).required(),
     });
   }

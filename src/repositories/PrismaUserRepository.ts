1367b0135a9d68197dc3b7fb47b16b2ed615b0c0
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-12 20:50:40 -0300

    feat: PrismaUserRepository

---
+++ src/repositories/PrismaUserRepository.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import { IUserDTO } from '../interfaces/IUser';
+import { IUserRepository } from './IUserRepository';
+
+class PrismaUserRepository implements IUserRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  public create = async (user: IUserDTO) => {
+    const newUser = await this.prisma.user.create({ data: user });
+
+    return newUser;
+  };
+
+  public findByUsername = async (username: string) => {
+    const user = await this.prisma.user.findUnique({ where: { username } });
+
+    return user;
+  };
+}
+
+export default PrismaUserRepository;

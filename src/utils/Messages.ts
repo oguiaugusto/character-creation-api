2100f9914a9c98b295b2e583d320c9e19ad2e672
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-14 19:34:46 -0300

    chore: new error messages

--- src/utils/Messages.ts
+++ src/utils/Messages.ts
@@ -4,6 +4,10 @@ enum Messages {
   USER_ALREADY_EXISTS = 'User already exists',
   USER_NOT_FOUND_USERNAME = 'User not found, check your username',
   INVALID_PASSWORD = 'Invalid password',
+  TOKEN_NOT_PROVIDED = 'Token not provided',
+  INVALID_TOKEN = 'Invalid token',
+  TOKEN_EXPIRED = 'Token expired',
+  STORY_WITH_SAME_TITLE = 'You already have a story with this title',
 }
 
 export default Messages;

77f72dffff4ebdea7b14e5613cc4d9b0c15cee89
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-14 21:09:04 -0300

    chore(test): CreateChaiRequest has postWithToken method

--- src/__tests__/utils/CreateChaiRequest.ts
+++ src/__tests__/utils/CreateChaiRequest.ts
@@ -5,6 +5,10 @@ class CreateChaiRequest {
   public static post = (endpoint: string) => async (body: any = {}) => {
     return chai.request(app).post(endpoint).send(body);
   }
+
+  public static postWithToken = (endpoint: string, token: string) => async (body: any = {}) => {
+    return chai.request(app).post(endpoint).set('Authorization', token).send(body);
+  }
 }
 
 export default CreateChaiRequest;

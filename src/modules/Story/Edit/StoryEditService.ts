44cfe07673268c53dccb8a248adcd7e22801a6e6
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-16 14:48:16 -0300

    feat(story edit): same title check
    it's not possible edit title to an existing one from the same author

--- src/modules/Story/Edit/StoryEditService.ts
+++ src/modules/Story/Edit/StoryEditService.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import { IStory, IStoryDTO } from '../../../interfaces/IStory';
 import { IStoryRepository } from '../../../repositories/Story/IStoryRepository';
 import { Messages, RequestError } from '../../../utils';
+import StoryUtils from '../utils';
 
 export interface IStoryEditService {
   handle(id: string, fields: Partial<IStoryDTO>): Promise<IStory>;
@@ -16,6 +17,15 @@ class StoryEditService {
     const existingStory = await this.repository.findById(id);
     if (!existingStory) {
       throw new RequestError(Messages.STORY_NOT_FOUND, StatusCodes.NOT_FOUND);
+    }
+
+    if (fields.title) {
+      const authorHasStoryWithSameTitle = await StoryUtils
+        .checkSameTitle(this.repository, fields.title, existingStory.authorId);
+
+      if (authorHasStoryWithSameTitle) {
+        throw new RequestError(Messages.STORY_WITH_SAME_TITLE, StatusCodes.CONFLICT);
+      }
     }
 
     const editedStory = await this.repository.editFields(id, fields);
